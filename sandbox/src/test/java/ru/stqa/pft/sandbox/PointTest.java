cc58cd5bb709192682813f446d7af6cbf810bef9
gordienko_ani <andruxagi@mail.ru>
2020-08-29 18:29:42 +0300

    Добавлен тестовый проект addressbook и соответствующие настройки Написан тест, на подобие Лекции 2.2

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+    @Test
+    public void testDistance(){
+        Point p1 = new Point(23,32);
+        Point p2 = new Point(54, 33);
+
+        double distance;
+        double x = p2.x - p1.x;
+        double y = p2.y - p1.y;
+        distance = Math.sqrt(x*x - y*y);
+
+        Assert.assertEquals(p1.distance(p2), distance);
+    }
+}
