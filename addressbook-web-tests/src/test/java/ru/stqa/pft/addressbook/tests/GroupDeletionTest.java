dae366a42f6ce6d95fd7fe50e393b41a55253cb9
gordienko_ani <andruxagi@mail.ru>
2020-09-01 08:59:47 +0300

    добавились новые пакеты разнесены классы по деррикториям создан вспомогательеные классс наследование заменено на делегирование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.tests.TestBase;
+
+
+public class GroupDeletionTest extends TestBase {
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroups();
+    app.returnToGroupPage();
+  }
+
+}
