a72813c8d9fa9f3fd16ecfed433ba00af973d18b
gordienko_ani <andruxagi@mail.ru>
2020-09-03 09:03:41 +0300

    добавлен тест GroupModificationTest добавлены новые метода в класс помощник GroupHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase{
+
+    @Test
+    public void testGroupModificationTest(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("1test", "2test", "3test"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
