1ede9973d86acefc3f2756e61d31d55f8cdd0c01
gordienko_ani <andruxagi@mail.ru>
2020-09-08 18:44:38 +0300

    оптимизированные переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -21,7 +21,7 @@ public class ContactHelper  extends HelperBase {
         type(By.name("email"),contactData.getEmail() );
 
         if (creation){
-            new Select(wd.findElement(By.name("new_group"))).deselectByVisibleText(ContactData.getGroup());
+            new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(ContactData.getGroup());
         } else {
             Assert.assertFalse(isElementPresent(By.name("new_group")));
         }
