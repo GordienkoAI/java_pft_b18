b1bf8e9a48df12dc70b112d52124ca753c833f6e
gordienko_ani <andruxagi@mail.ru>
2020-10-09 13:23:22 +0300

    Реализовано создание json файла для Контактов и загрузка данных из этого файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,9 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -33,7 +37,23 @@ public class ContactCreationTest extends TestBase {
         return  contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
     }
 
-  @Test(dataProvider = "validContacts")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts =  gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); // List<ContactData>.class
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    }
+
+
+  @Test(dataProvider = "validGroupsFromJson")
   public void testCreateNewContact(ContactData cont) throws Exception {
      Contacts before = app.contact().all();
       app.contact().init();
