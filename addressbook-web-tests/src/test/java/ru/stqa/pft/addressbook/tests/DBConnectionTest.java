4dbdd845e7a51d85f84cf0f697e9b237ed3e0115
gordienko_ani <andruxagi@mail.ru>
2020-10-11 23:10:51 +0300

    Подключение и запуск Hibernate к БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DBConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DBConnectionTest.java
@@ -17,8 +17,8 @@ public class DBConnectionTest {
             ResultSet rs =st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
             Groups groups = new Groups();
             while(rs.next()){
-                new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
-                        .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer"));
+               groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
             }
             rs.close();
             st.close();
