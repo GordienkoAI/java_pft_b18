41029c2dbe06f2b079f8cc8930d10699380f3fae
gordienko_ani <andruxagi@mail.ru>
2020-10-28 17:22:46 +0300

    Собрал в один тест проверки по телефону, адресу и email клиенту, в тест testContactInfo

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactInfoTest.java
@@ -0,0 +1,69 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class ContactInfoTest extends TestBase{
+
+    @Test
+    public void testContactInfo(){
+        app.goTo().gotoHomePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+        assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm));
+        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    }
+
+
+    @Test
+    public void testContactPhones(){
+        app.goTo().gotoHomePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+        assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+    }
+
+    @Test
+    public void contactAddressTest(){
+        app.goTo().gotoHomePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+        assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm));
+    }
+
+    @Test
+    public void contactMailTest(){
+        app.goTo().gotoHomePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+        assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    }
+
+    private String mergeEmails(ContactData contact) {
+        return Arrays.asList(contact.getEmail(), contact.getEmail1(), contact.getEmail2())
+                .stream().filter((s) -> ! s.equals(""))
+                .map(ContactMailTest::cleaned)
+                .collect(Collectors.joining("\n"));
+    }
+
+
+
+    private  String mergePhones(ContactData contact) {
+        return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+                .stream().filter((s)-> ! s.equals(""))
+                .map(ContactsPhoneTests::cleaned)
+                .collect(Collectors.joining("\n"));
+    }
+
+    public static String cleaned(String phone){
+        return phone.replaceAll("\\s","").replaceAll("[-()]","");
+    }
+
+}
